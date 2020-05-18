449135a78999b4f4d28b97ddeaaf5d4df25a92f7
connor <vbrookesy@hotmail.co.uk>
2020-05-18 13:59:30 +0100

    Added and rendered 3 new functional componants

--- src/App.js
+++ src/App.js
@@ -5,7 +5,18 @@ const AboutMe = () => {
   return(
     <div>
       <h1>Would you like to know more about me?</h1>
+      <p>Maybe you would, maybe you wouldn't</p>
     </div>
+  )
+}
+const whatShouldIWear = () => {
+  return(
+    <h1>What should you wear to different places?</h1>
+  )
+}
+const WhatsYourName = () => {
+  return(
+    <h1>Name?</h1>
   )
 }
 
@@ -15,6 +26,8 @@ class App extends Component{
       <div className="App">
         <h1>Hello world</h1>
         <AboutMe />
+        <WhatsYourName />
+        <whatShouldIWear />
       </div>
     )
   }
