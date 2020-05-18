f805b25d3e5ced6f2dc703bb2dedaec89a9f2ddf
connor <vbrookesy@hotmail.co.uk>
2020-05-18 13:42:47 +0100

    Functional class added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
+const AboutMe
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1>Hello world</h1>
       </div>
-      
-  );
+    )
+  }
 }
 
 export default App;
